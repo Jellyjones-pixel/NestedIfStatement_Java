bfceb55a22b3b70c6a09d0f0e7bb7c086f0bd1a4
JOHN MBITHI MUTAVE <83606270+Jellyjones-pixel@users.noreply.github.com>
2023-09-27 16:40:39 +0100

    Add files via upload
    Updated

---
+++ NestedIfStatementExample1/src/nestedifstatementexample1/NestedIfStatementExample1.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package nestedifstatementexample1;
+
+/**
+ *
+ * @author jelly_joneske
+ */
+public class NestedIfStatementExample1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+ int age = 17; 
+boolean hasVoterCard = true; 
+// Evaluating the expression that will return true or false 
+if (age > 18) 
+{ 
+// If outer condition is true then this condition will be check 
+    if (hasVoterCard) 
+{ 
+System.out.println("You are Eligible"); 
+
+    }
+}
+    }}
+
